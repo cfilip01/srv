f9413beb6f337287e8d5b7998f0a32a1bbd25ef1
Cosmin Filip <cfilip@neverfail.com>
2017-08-14 15:19:32 +0300

    sadsa

--- salt/script.sh
+++ salt/script.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-folder = "$HOME/cosmin"
+read -p "enter folder name: " fol
+folder = "$fol"
 if [[ -d $folder ]]; then
   if mkdir "$HOME/cosmin"; then
     echo "created directory"
@@ -7,3 +8,4 @@ else
   echo "could not create directory"
   exit 1
   fi
+fi
