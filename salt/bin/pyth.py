af386cf1283bec2e94f25466eec0f9ee4c14a1fd
Cosmin Filip <cfilip@neverfail.com>
2017-08-19 19:57:57 +0300

    new file added

---
+++ salt/bin/pyth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+def HelloWorld():
+    print("Hello World")
+    myName = 'Cosmin'
+    if (myName == "CosMin"):
+        print("Cosmin the greatest")
+    else:
+        print("Cosmin is wrong typed")
+HelloWorld()
+
+def adunare(nr1, nr2):
+    return nr1 + nr2
+
+def impartire(nr1, nr2)
+    return nr1 / nr2
+
+myValue = adunare(5, 9)
+print(myValue)
+
+num1 = input("Enter num1: ")
+print(num1)
